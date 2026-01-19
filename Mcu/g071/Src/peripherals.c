8fb55bff3c569ad70f7dbe63e24c8b0e072b0d4c
SergioDhe-DropLab <134008599+SergioDhe-DropLab@users.noreply.github.com>
2026-01-19 12:59:00 -0500

    clean up clock init

--- Mcu/g071/Src/peripherals.c
+++ Mcu/g071/Src/peripherals.c
@@ -60,22 +60,21 @@ void initAfterJump()
 void SystemClock_Config(void)
 {
     LL_FLASH_SetLatency(LL_FLASH_LATENCY_2);
-    if (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_2)
+    while (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_2)
     {
-        //  Error_Handler();
-    };
+    }
 
     /* HSI configuration and activation */
     LL_RCC_HSI_Enable();
     while (LL_RCC_HSI_IsReady() != 1)
     {
-    };
+    }
 
     /* LSI configuration and activation */
     LL_RCC_LSI_Enable();
     while (LL_RCC_LSI_IsReady() != 1)
     {
-    };
+    }
 
     /* Main PLL configuration and activation */
     LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLLM_DIV_1, 8,
@@ -84,7 +83,7 @@ void SystemClock_Config(void)
     LL_RCC_PLL_EnableDomain_SYS();
     while (LL_RCC_PLL_IsReady() != 1)
     {
-    };
+    }
 
     /* Set AHB prescaler*/
     LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
@@ -93,12 +92,11 @@ void SystemClock_Config(void)
     LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
     while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
     {
-    };
+    }
 
     /* Set APB1 prescaler*/
     LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
     LL_Init1msTick(64000000);
-    LL_SetSystemCoreClock(64000000);
     /* Update CMSIS variable (which can be updated also through
      * SystemCoreClockUpdate function) */
     LL_SetSystemCoreClock(64000000);
